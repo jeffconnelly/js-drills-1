c446b3eb9231dfa4581fcdf533d51392f47e4867
Brent Guistwite <brentguistwite@gmail.com>
2017-12-04 14:50:29 -0500

    added year of birth function

--- drills.js
+++ drills.js
@@ -2,9 +2,14 @@
 
 
 function whoAmI(name, age) {
-  const yearOfBirth = 2017 - age;
-  console.log(`Hi my name is ${name} and I'm ${age} years old`);  
+  
+console.log(`Hi my name is ${name} and I'm ${age} years old`);  
   console.log(`I was born in ${yearOfBirth}`);
 }
+
+function yearOfBirth(age){
+  return 2017 - age;
+}
+
 
 whoAmI('Jeff', 26);
