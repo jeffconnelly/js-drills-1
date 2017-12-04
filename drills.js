28577bf87827a328d38daa64fe2db92518e44850
Brent Guistwite <brentguistwite@gmail.com>
2017-12-04 15:58:11 -0500

    added conditionals to catch errors

--- drills.js
+++ drills.js
@@ -2,13 +2,25 @@
 
 
 function whoAmI(name, age) {
+  if (typeof name !== "string" || typeof age !== "number" ){
+    console.error("Arguments not valid");
+    return;
+  }
   console.log(`Hi my name is ${name} and I'm ${age} years old`);
-  const yob = yearOfBirth(age);
-  console.log(`I was born in ${yob}`);
+  try{
+    const yob = yearOfBirth(age);
+    console.log(`I was born in ${yob}`);
+  } catch(e){
+    console.log(e.message);
+  }
 }
 
 function yearOfBirth(age){
+  if (age <= 0) throw new Error("Age can not be negative");
  return 2017-age;
 }
 
-whoAmI('Jeff', 5);
+whoAmI("Jeff", "twen");
+
+
+
